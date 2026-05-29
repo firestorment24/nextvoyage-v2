bcee22e0ad973a18d754963a032e44f81d74a8f0
firestorment24 <daryl.clark@fora.travel>
2026-05-29 18:14:29 -0400

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,21 +2,21 @@ import './globals.css'
 import Navigation from './Navigation' 
 
 export const metadata = {  
-  title: 'NexVoyage Collective',  
-  description: 'The website engine for NexVoyage Collective',  
+title: 'NexVoyage Collective',  
+description: 'The website engine for NexVoyage Collective',  
 }
 
 export default function RootLayout({  
-  children,  
+children,  
 }: {  
-  children: React.ReactNode  
+children: React.ReactNode  
 }) {  
-  return (  
-    <html lang="en">  
-      <body className="bg-black">  
-        <Navigation />  
-        {children}  
-      </body>  
-    </html>  
-  )  
+return (  
+  <html lang="en">  
+    <body className="bg-black">  
+      <Navigation />  
+      {children}  
+    </body>  
+  </html>  
+)  
 }  
