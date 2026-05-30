2bf339d229617f636eb80b6e30b9647ae1ba0226
firestorment24 <daryl.clark@fora.travel>
2026-05-30 02:17:10 -0400

    Change Navigation import to default import

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,6 @@
 import './globals.css'  
 import { Inter } from 'next/font/google'  
-import { Navigation } from './Navigation'  
+import Navigation from './Navigation'  
 import Footer from './Footer'
 
 const inter = Inter({ subsets: ['latin'] })
