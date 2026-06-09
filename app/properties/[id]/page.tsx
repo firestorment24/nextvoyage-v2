8ffc6f1be3bbefee4fb319d6fa497cdd2a717efc
firestorment24 <daryl.clark@fora.travel>
2026-06-09 16:32:28 -0400

    Update page.tsx

--- app/properties/[id]/page.tsx
+++ app/properties/[id]/page.tsx
@@ -8,7 +8,7 @@ import Link from "next/link";
 interface Props {  
   params: { id: string };  
 }
-
+ 
 export async function generateStaticParams() {  
   return properties.map((prop) => ({  
     id: prop.id,  
