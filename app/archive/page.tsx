8b43968f0a1c254b9f216e6c39b3bc5ee993c4ed
firestorment24 <daryl.clark@fora.travel>
2026-06-13 19:41:54 -0400

    Fix formatting in page.tsx

--- app/archive/page.tsx
+++ app/archive/page.tsx
@@ -15,7 +15,7 @@ return (
           Property Archive  
         </h2>  
       </div>  
-        
+         
       <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">  
         {PROPERTY_DATA.map((property) => (  
           <Link   
