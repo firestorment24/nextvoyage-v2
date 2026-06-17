fec195422d06834bd04b450211c0f879f689c66c
firestorment24 <daryl.clark@fora.travel>
2026-06-16 22:26:47 -0400

    Update Concierge link to point to '/inquiry'

--- components/Navigation.tsx
+++ components/Navigation.tsx
@@ -20,7 +20,7 @@ const Navigation = () => {
     { name: 'HOME', href: '/', sub: 'The Beginning' },  
     { name: 'THE ARCHIVE', href: '/archive', sub: 'Curated Collection' },  
     { name: 'JOURNAL', href: '/journal', sub: 'Travel Stories' },  
-    { name: 'CONCIERGE', href: '/concierge', sub: 'Direct Inquiry' },  
+    { name: 'CONCIERGE', href: '/inquiry', sub: 'Direct Inquiry' },  
   ];
 
   const brass = '#C5A059';  
