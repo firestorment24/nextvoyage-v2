fe5dea9e2b98ee8571fa0b2a21481cebabc70116
firestorment24 <daryl.clark@fora.travel>
2026-06-25 00:37:03 -0400

    Update image remote patterns in next.config.ts
    Added new remote patterns for image loading.

--- next.config.ts
+++ next.config.ts
@@ -1,40 +1,44 @@
 /** @type {import('next').NextConfig} */  
 const nextConfig = {  
-images: {  
-  remotePatterns: [  
-    {  
-      protocol: 'https',  
-      hostname: 'dynamic-media-cdn.tripadvisor.com',  
-      pathname: '/**',  
-    },  
-    {  
-      protocol: 'https',  
-      hostname: 'www.aman.com',  
-      pathname: '/**',  
-    },  
-    {  
-      protocol: 'https',  
-      hostname: 'images.luxuryhotelsmag.com',  
-      pathname: '/**',  
-    },  
-    {  
-      protocol: 'https',  
-      hostname: 'www.ourhabitas.com',  
-      pathname: '/**',  
-    },  
-    {  
-      protocol: 'https',  
-      hostname: 'images.unsplash.com',  
-      pathname: '/**',  
-    } , 
-
-     {  
-      protocol: 'https',  
-      hostname: 'cdn.marblism.com',  
-      pathname: '/**',  
-    }  
-  ],  
-},  
-};
+  images: {  
+    remotePatterns: [  
+      {  
+        protocol: 'https',  
+        hostname: 'dynamic-media-cdn.tripadvisor.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'www.aman.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'images.luxuryhotelsmag.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'www.ourhabitas.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'images.unsplash.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: 'cdn.marblism.com',  
+        pathname: '/**',  
+      },  
+      {  
+        protocol: 'https',  
+        hostname: '*.private.blob.vercel-storage.com',  
+        pathname: '/**',  
+      },  
+    ],  
+  },  
+}
 
 export default nextConfig;  
