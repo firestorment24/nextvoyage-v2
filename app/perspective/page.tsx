f18db9c17ec591dbfb98d677c29a1ed566c2e872
firestorment24 <daryl.clark@fora.travel>
2026-07-14 00:51:36 -0400

    Remove Footer import from page.tsx

--- app/perspective/page.tsx
+++ app/perspective/page.tsx
@@ -1,7 +1,7 @@
 import { sql } from '@vercel/postgres'  
 import Link from 'next/link'  
 import Navigation from '@/components/Navigation'  
-import Footer from '@/components/Footer'
+
  
 export const revalidate = 0  
 export const dynamic = 'force-dynamic'
