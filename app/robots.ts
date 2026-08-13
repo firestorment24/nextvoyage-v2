8d1c500ff73ec6fe18ae1656785b56527343c098
firestorment24 <daryl.clark@fora.travel>
2026-08-13 03:26:36 -0400

    Refactor robots function to use array for rules

--- app/robots.ts
+++ app/robots.ts
@@ -4,10 +4,23 @@ const BASE_URL = 'https://nexvoyagecollective.com'
 
 export default function robots(): MetadataRoute.Robots {  
 return {  
-rules: {  
-userAgent: '*',  
-allow: '/',  
-},  
+rules: [  
+  {  
+    userAgent: '*',  
+    allow: '/',  
+    disallow: [  
+      '/admin/',  
+      '/api/',  
+      '/terminal/',  
+      '/lead/',  
+      '/manifest/',  
+      '/consent/',  
+      '/success/',  
+      '/the-reserve.html',  
+      '/ontact/',  
+    ],  
+  },  
+],  
 sitemap: `${BASE_URL}/sitemap.xml`,  
 host: BASE_URL,  
 }  
